f3f0ff06aea7597bd96322257adb2e93b3c6bf72
Susanmwende <mwendezmasaku@gmail.com>
2024-03-25 08:34:04 +0300

    assessment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){
+ text("Barnie bakes brown bagels and buns")
+    println(numbers())
+}
+fun text(sentence:String){
+    println(sentence)
+
+}
+
+
+fun numbers(arrayOf:Array<Int>): Array<Int> {
+    var sum = arrayOf(20,30,40,50)
+    return sum
+
+}
+fun isPalindrome(word:String):Boolean{
+    if (word==)
+
+}
